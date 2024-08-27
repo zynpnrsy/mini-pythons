5d0978a378253a3f48b7dc958a19360f76c21292
zynpnrsy <119500158+zynpnrsy@users.noreply.github.com>
2024-08-27 19:31:01 +0300

    Add files via upload

---
+++ reverse_sentences.py
@@ -0,0 +1,14 @@
+
+#reverse sentences and words by one by
+
+def reverse():
+    sentence = input("Enter a sentence: ")
+    reverse_sentence = sentence[::-1]
+    
+    print("Original version:    ",sentence)
+    print("Reverse version:     ",reverse_sentence)
+    
+     
+     
+     
+reverse()
